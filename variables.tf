a18f4411145d5310ef5d8844833bd83b843c93a0
fuckerlo <88121812+fuckerlo@users.noreply.github.com>
2022-06-15 18:35:03 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,12 @@
 variable "instance_number" {
  default = 2
- descrpition = "实例数量"
 }
 variable "cpu_core_count" {
  default = 1
- descrpition = "CPU规格"
 }
 variable "memory_size" {
  default = 2
- descrpition = "内存规格"
 }
 variable "image_id" {
  default = "centos_7_6_x64_20G_alibase_20211130.vhd"
- description = "镜像id"
 }
